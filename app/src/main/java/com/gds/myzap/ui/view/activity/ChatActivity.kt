4b3e1525b0f576de35a6fb4fa60ccd25a3b4826a
Gabriel Do Carmo <83257171+G7Carmo@users.noreply.github.com>
2022-03-28 18:58:17 -0300

    Tela de chat visualmente concluida

--- app/src/main/java/com/gds/myzap/ui/view/activity/ChatActivity.kt
+++ app/src/main/java/com/gds/myzap/ui/view/activity/ChatActivity.kt
@@ -1,9 +1,11 @@
 package com.gds.myzap.ui.view.activity
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gds.myzap.R
 import com.gds.myzap.databinding.ActivityChatBinding
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var binding : ActivityChatBinding
@@ -12,21 +14,15 @@ class ChatActivity : AppCompatActivity() {
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initToolBar()
-        listners()
     }
 
 
+    @SuppressLint("ResourceType")
     private fun initToolBar() {
-        val toolbar = binding.toolbarChat.toolbar
-        toolbar.title = "MyZap"
+        val toolbar = binding.toolbarChat
+        toolbar.title = ""
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-
-    private fun listners() = with(binding) {
-        fab.setOnClickListener {
-
-        }
-   }
 }
